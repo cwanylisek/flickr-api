fe888e800475468e332955410fd70134105cae50
Bartek Tylkowski <rewiks@gmail.com>
2019-12-08 21:41:36 +0100

    redux-thunk

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import actions from './actions';
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+store.dispatch(actions.add('testtt'));
+
+window.store = store
+
+export default store;
